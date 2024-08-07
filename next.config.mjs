0dc60d4546010fcea5077da936002b4dfe86c6c0
Grigor <grdimitrov@gmail.com>
2024-08-07 16:36:16 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -10,8 +10,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
 },
-basePath: '',
-assetPrefix: '',
   // Configure `pageExtensions`` to include MDX files
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   // Optionally, add any other Next.js config below
